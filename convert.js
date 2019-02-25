2a1d8cc80df13144d380022b5c30d773184c8e4e
MJBaumli <michaeljbaumli@gmail.com>
2019-02-25 17:53:29 -0600

    Things are working.  Sort of

---
+++ convert.js
@@ -0,0 +1,13 @@
+function convert(x) {
+    return x*0.453592
+    }
+
+QUnit.test("convert", function(assert) {
+	assert.equal(convert(0), 0, "0 lb == 0 kg");
+	assert.equal(convert(1), 0.453592, "1 lb == 0.43592");
+	assert.equal(convert(2), 0.907184, "2 lb == 0.907184");
+	assert.equal(convert(3), 1.360776, "3 lb == 1.360776");
+	assert.equal(convert(10), 4.53592, "10 lb == 4.53592");
+	assert.equal(convert(332549), 150841.566008, "332549 lb == 150841.566008");
+	assert.equal(convert(-1), -0.453592, "-1 lb == -0.43592");
+ });
