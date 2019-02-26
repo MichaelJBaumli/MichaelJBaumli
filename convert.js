4d930b6a3e57e5d6f12e5eaab7e81f42308c5137
MJBaumli <michaeljbaumli@gmail.com>
2019-02-25 18:53:48 -0600

    QUnit Test Works

--- convert.js
+++ convert.js
@@ -1,22 +1,39 @@
 
 console.log('SCRIPT START')
-window.addEventListener('load', (event) =>{
-console.log('Declare testable functions.....................')
 
 function convert(x) {
-return x*0.453592
+    if(typeof x !== 'number'){
+        throw Error("this is not a number")
+    }
+    return x * 0.453592
 }
+window.addEventListener('load', (event) => {
+    console.log('Declare testable functions.....................')
 
-console.log('Defined convert=' + convert)
+    function convert(x) {
 
-console.log('Declare event listeners .......................')
+        return x * 0.453592
+    }
 
-document.getElementById('clicker').addEventListener('click', function () {
- const i = parseInt(document.getElementById('weight').value)
- console.log('i=' + i)
- const ans =  'This weight in kilograms is ' + convert(i) + '.'
-document.getElementById('result').innerHTML = ans
-})
+    console.log('Defined convert=' + convert)
+
+    console.log('Declare event listeners .......................')
+
+    function callfunc() {
+
+        const i = parseInt(document.getElementById('weight').value)
+        console.log('i=' + i)
+        const ans = 'This weight in kilograms is ' + convert(i) + '.'
+        document.getElementById('result').innerHTML = ans
+    }
+
+
+    // document.getElementById('clicker').addEventListener('click', function () {
+    //     const i = parseInt(document.getElementById('weight').value)
+    //     console.log('i=' + i)
+    //     const ans = 'This weight in kilograms is ' + convert(i) + '.'
+    //     document.getElementById('result').innerHTML = ans
+    // })
 
 })
 
--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
-QUnit.test("convert", function(assert) {
+
+
+QUnit.test("Here's a test that should always pass", function (assert) {
+	assert.ok(1 <= "3", "1<3 - the first agrument is 'truthy', so we pass!");
+});
+
+QUnit.test("convert", function (assert) {
 	assert.equal(convert(0), 0, "0 lb == 0 kg");
 	assert.equal(convert(1), 0.453592, "1 lb == 0.43592");
 	assert.equal(convert(2), 0.907184, "2 lb == 0.907184");
 	assert.equal(convert(3), 1.360776, "3 lb == 1.360776");
 	assert.equal(convert(10), 4.53592, "10 lb == 4.53592");
 	assert.equal(convert(332549), 150841.566008, "332549 lb == 150841.566008");
-    assert.equal(convert(-1), -0.453592, "-1 lb == -0.43592");
-    assert.throws(function(){convert("bob")}, /this is not a number/, "passing null raises an exception")
- });
+	assert.equal(convert(-1), -0.453592, "-1 lb == -0.43592");
+	assert.throws(function () { convert("bob") }, /this is not a number/, "passing null raises an exception")
+});
